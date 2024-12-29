ed19b89644eede471feba47c79cf81588cf1a089
Aditya <adityapermono8@gmail.com>
2024-12-29 22:20:37 +0700

    Membuat TopAppBar untuk menampilkan app bar dengan judul, ikon kembali, dan ikon refresh

---
+++ app/src/main/java/com/example/p12service/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,41 @@
+package com.example.p12service.ui.customwidget
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        title = {Text(title)},
+        actions = {
+            Icon(imageVector = Icons.Default.Refresh,
+                contentDescription = "",
+                modifier = Modifier.clickable {
+                onRefresh()
+            })
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if (canNavigateBack) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
+}
