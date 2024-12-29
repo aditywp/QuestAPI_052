67d232008125ee90cabafe06312e81eddb67cbd4
Aditya <adityapermono8@gmail.com>
2024-12-30 00:25:36 +0700

    Membuat DestinasiEntry untuk navigasi dan judul screen "Entry Mhs"

---
+++ app/src/main/java/com/example/p12service/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.p12service.ui.view
+
+import com.example.p12service.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
