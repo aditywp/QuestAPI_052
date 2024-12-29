ccb11e7acfba733d31a44a58a77993cc1351ab78
Aditya <adityapermono8@gmail.com>
2024-12-30 00:40:44 +0700

    Membuat EntryBody untuk menampilkan form input mahasiswa dan tombol simpan

--- app/src/main/java/com/example/p12service/ui/view/InsertView.kt
+++ app/src/main/java/com/example/p12service/ui/view/InsertView.kt
@@ -1,19 +1,31 @@
 package com.example.p12service.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import com.example.p12service.ui.customwidget.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.p12service.ui.navigation.DestinasiNavigasi
+import com.example.p12service.ui.viewmodel.InsertUiEvent
+import com.example.p12service.ui.viewmodel.InsertUiState
 import com.example.p12service.ui.viewmodel.InsertViewModel
 import com.example.p12service.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
@@ -59,3 +71,33 @@ fun EntryMhsScreen(
         )
     }
 }
+
+
+//1
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: ()-> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
