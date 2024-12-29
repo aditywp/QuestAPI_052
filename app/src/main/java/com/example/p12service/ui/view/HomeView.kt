3590bdab8e434f818ad56b57e21777c38f51c991
Aditya <adityapermono8@gmail.com>
2024-12-30 00:48:59 +0700

    membuat objek DestinasiHome untuk navigasi ke home screen mahasiswa

---
+++ app/src/main/java/com/example/p12service/ui/view/HomeView.kt
@@ -0,0 +1,10 @@
+package com.example.p12service.ui.view
+
+import com.example.p12service.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
+
