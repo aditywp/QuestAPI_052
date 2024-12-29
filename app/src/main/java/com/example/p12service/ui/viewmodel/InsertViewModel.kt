7f40ed92617ff9e18617f977f7ca1d8ccf92d3ea
Aditya <adityapermono8@gmail.com>
2024-12-29 21:53:10 +0700

    Menambahkan fungsi InsertUiEvent menjadi Mahasiswa

--- app/src/main/java/com/example/p12service/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p12service/ui/viewmodel/InsertViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.p12service.ui.viewmodel
 
+import com.example.p12service.model.Mahasiswa
+
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
@@ -11,4 +13,13 @@ data class InsertUiEvent(
     val jenisKelamin: String = "",
     val kelas: String = "",
     val angkatan: String= ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
 )
