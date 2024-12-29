3498620cc7dbecd6238f42c8f7bd391dd0b24c71
Aditya <adityapermono8@gmail.com>
2024-12-29 21:06:06 +0700

    Membuat Retrofit dengan baseurl dan konfigurasi json

--- app/src/main/java/com/example/p12service/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/p12service/dependenciesinjection/MahasiswaContainer.kt
@@ -1,13 +1,24 @@
 package com.example.p12service.dependenciesinjection
 
 import com.example.p12service.repository.MahasiswaRepository
-
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer {
     val kontakRepository: MahasiswaRepository
 }
 
 class MahasiswaContainer: AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:8000/umyTI/" //localhost diganti ip kalo run di hp
+
+    private val json = Json { ignoreUnknownKeys = true }
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
 
 
 }
