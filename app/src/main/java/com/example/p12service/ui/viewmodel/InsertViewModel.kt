463eb00ecf002b907ca213798764589d6b30d86e
Aditya <adityapermono8@gmail.com>
2024-12-29 21:48:57 +0700

    membuat data class InsertUiState dan InsertUiEvent untuk menampung state UI form mahasiswa

---
+++ app/src/main/java/com/example/p12service/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.p12service.ui.viewmodel
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String= ""
+)
