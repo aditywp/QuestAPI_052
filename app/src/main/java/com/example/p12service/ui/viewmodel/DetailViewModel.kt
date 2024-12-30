9f15891e9fafd837a58d5831c74b442e2041dbcb
Aditya <adityapermono8@gmail.com>
2024-12-30 11:09:53 +0700

    Menambahkan ViewModel untuk mengelola detail mahasiswa, termasuk pengambilan dan penghapusan data berdasarkan NIM.

---
+++ app/src/main/java/com/example/p12service/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,87 @@
+package com.example.p12service.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.p12service.model.Mahasiswa
+import com.example.p12service.repository.MahasiswaRepository
+import com.example.p12service.ui.view.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+) : ViewModel() {
+    private val nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    var detailUiState: DetailUiState by mutableStateOf(DetailUiState())
+        private set
+
+    init {
+        getMahasiswaByNim()
+    }
+
+    fun getMahasiswaByNim() {
+        viewModelScope.launch {
+            detailUiState = DetailUiState(isLoading = true)
+            try {
+                val result = mahasiswaRepository.getMahasiswabyNim(nim)
+                detailUiState = DetailUiState(
+                    detailUiEvent = result.toDetailUiEvent(),
+                    isLoading = false
+                )
+            } catch (e: Exception) {
+                detailUiState = DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown"
+                )
+            }
+        }
+    }
+
+    fun deleteMhs() {
+        viewModelScope.launch {
+            detailUiState = DetailUiState(isLoading = true)
+            try {
+                mahasiswaRepository.deleteMahasiswa(nim)
+
+                detailUiState = DetailUiState(isLoading = false)
+            } catch (e: Exception) {
+                detailUiState = DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: " Error"
+                )
+            }
+        }
+    }
+}
+
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiEvent{
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
