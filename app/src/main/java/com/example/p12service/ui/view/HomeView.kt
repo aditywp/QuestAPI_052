41ea6b976d9ed7a0a625cd837da6665cef0ad544
Aditya <adityapermono8@gmail.com>
2024-12-30 09:31:40 +0700

    membuat fungsi MhsLayout untuk menampilkan daftar mahasiswa dalam bentuk list

--- app/src/main/java/com/example/p12service/ui/view/HomeView.kt
+++ app/src/main/java/com/example/p12service/ui/view/HomeView.kt
@@ -1,13 +1,17 @@
 package com.example.p12service.ui.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -151,3 +155,30 @@ fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun MhsLayout(
+    mahasiswa: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Mahasiswa) -> Unit,
+    onDeleteClick: (Mahasiswa) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(mahasiswa) { kontak ->
+            MhsCard(
+            mahasiswa = kontak,
+                modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onDetailClick(kontak) },
+                onDeleteClick = {
+                    onDeleteClick(kontak)
+                }
+            )
+        }
+    }
+}
+
