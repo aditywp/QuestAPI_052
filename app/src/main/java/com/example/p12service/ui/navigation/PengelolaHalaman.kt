71932314d08e6f4eac3c2f70c2bb6708076b0628
Aditya <adityapermono8@gmail.com>
2024-12-30 13:03:45 +0700

    Menambahkan navigasi untuk halaman Detail dan Update Mahasiswa dengan argument NIM.

--- app/src/main/java/com/example/p12service/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/p12service/ui/navigation/PengelolaHalaman.kt
@@ -3,23 +3,32 @@ package com.example.p12service.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.p12service.ui.view.DestinasiDetail
 import com.example.p12service.ui.view.DestinasiEntry
 import com.example.p12service.ui.view.DestinasiHome
+import com.example.p12service.ui.view.DestinasiUpdate
+import com.example.p12service.ui.view.DetailView
 import com.example.p12service.ui.view.EntryMhsScreen
 import com.example.p12service.ui.view.HomeScreen
+import com.example.p12service.ui.view.UpdateView
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
     NavHost(
-        navController = navController, startDestination = DestinasiHome.route, modifier = Modifier,
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
     ) {
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
-                onDetailClick = {
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
                 }
             )
         }
@@ -32,6 +41,45 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM){
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let {
+                DetailView(
+                    NavigateBack = {
+                        navController.navigate(DestinasiHome.route) {
+                            popUpTo(DestinasiHome.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    onEditClick =  {navController.navigate("${DestinasiUpdate.route}/$it")},
+                    onDeleteClick = { navController.popBackStack()
+                    }
+                )
+            }
+        }
+        composable(
+            DestinasiUpdate.routesWithArg,
+            arguments = listOf(navArgument(DestinasiDetail.NIM){
+            type = NavType.StringType
+        }
+        )
+        ){
+            val nim = it.arguments?.getString(DestinasiUpdate.NIM)
+            nim?.let { nim ->
+                UpdateView(
+                    onNavigateBack = { navController.popBackStack() },
+                    onNavigate = { navController.popBackStack() }
+                )
+            }
         }
     }
 }
